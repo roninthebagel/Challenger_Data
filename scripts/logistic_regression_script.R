0d7fa6980fa5fb5d0f2f4cac3fecc11eb71712cd
roninthebagel <ronin@run2online.com>
2025-03-04 11:42:12 +0000

    made plots to compare temperature and o ring failure

--- scripts/logistic_regression_script.R
+++ scripts/logistic_regression_script.R
@@ -1,4 +1,35 @@
 # read csv file
 challenger <- read_csv("data/Challenger.csv")
 
-# 
+# filtering data - to only include flights where a failure occured
+o_ring_temp_plot <- challenger %>% 
+  filter(oring_dt > 0) %>% 
+  ggplot(aes(y=oring_dt, x=temp))+geom_point()+
+  ggtitle("Temperature on flight launches where an O-ring incident occurred")
+print(o_ring_temp_plot)
+
+ggsave("figures/o_ring_temp_plot", 
+       plot = o_ring_temp_plot, 
+       width = 15, 
+       height = 10, 
+       units = "cm", 
+       device = "pdf")
+
+# using all launch data
+all_launch_temp_plot <- challenger |> 
+  ggplot(aes(y=oring_dt, 
+             x=temp))+
+  geom_point()+
+  geom_smooth(method="lm")+
+  ggtitle("All launch data")
+print(all_launch_temp_plot)
+
+ggsave("figures/all_launch_temp_plot", 
+       plot = all_launch_temp_plot, 
+       width = 15, 
+       height = 10, 
+       units = "cm", 
+       device = "pdf")
+
+# there is a clear relationship between the temperature and risk of o ring failure
+
